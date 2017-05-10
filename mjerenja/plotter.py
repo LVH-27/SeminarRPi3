1e4746b9110d01b801cf4f5be4caff0d7f810cfd
rincewind <leonard.volaric-horvat@fer.hr>
2017-05-10 11:33:56 +0200

    Started presentation

--- mjerenja/plotter.py
+++ mjerenja/plotter.py
@@ -1,5 +1,21 @@
 import matplotlib.pyplot as plt
 
+def lpf(axis_data):
+	aux = []
+	old_x = axis_data[0]
+	alpha = 0.2
+
+
+	for x in axis_data:
+
+		curr_data = (1 - alpha) * x + alpha * old_x
+		aux.append(curr_data)
+		old_x = curr_data
+
+
+	return aux
+
+
 X = []
 Y = []
 Z = []
@@ -7,7 +23,7 @@ C = []
 
 while(1):
 	try:
-		line = raw_input().split()
+		line = input().split()
 		if "detected" in line:
 			continue
 		elif "click" in line:
@@ -20,10 +36,18 @@ while(1):
 	except EOFError as e:
 		break
 
+X_lpf = lpf(X)
+
+plt.subplot(211)
 x = plt.plot(X, color="r", label="X axis")
-y = plt.plot(Y, color="g", label="Y axis")
-z = plt.plot(Z, color="b", label="Z axis")
-c = plt.plot(C, color="k", label="Click detection")
+plt.subplot(212)
+x_lpf = plt.plot(X_lpf, color="b", label="Filtered X axis")
+#plt.subplot(211)
+
+# y = plt.plot(Y, color="g", label="Y axis")
+# z = plt.plot(Z, color="b", label="Z axis")
+# c = plt.plot(C, color="k", label="Click detection")
+
 plt.ylabel("Relative acceleration")
 plt.xlabel("Time")
 #plt.legend(["X","Y","Z","Click detection"],bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
